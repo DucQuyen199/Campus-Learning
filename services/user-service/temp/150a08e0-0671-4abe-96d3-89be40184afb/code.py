72885c38d548381c10150281a0a881aa0865f40f
quyen nguyen <quyennguyen@MacBook-Air.local>
2025-07-24 19:43:51 +0700

    Remove temp folder in user-service

--- services/user-service/temp/150a08e0-0671-4abe-96d3-89be40184afb/code.py
+++
@@ -1,9 +0,0 @@
-# Yêu cầu người dùng nhập hai số
-a = float(input("Nhập số a: "))
-b = float(input("Nhập số b: "))
-
-# Tính tổng
-tong = a + b
-
-# In kết quả
-print("Tổng của", a, "và", b, "là:", tong)
